4db85d15ac3553c995e1da2deddab6d79bd9cd0a
Nuby <nuby.4register@gmail.com>
2023-07-22 17:18:06 +0300

    UserMapper refactoring

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -4,7 +4,6 @@ import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;

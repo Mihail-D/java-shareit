e1ebb213367f182ecac577b03c215407cf898257
Nuby <nuby.4register@gmail.com>
2023-08-01 10:01:25 +0300

    server-booking

---
+++ server/src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -0,0 +1,61 @@
+package ru.practicum.shareit.booking;
+
+import lombok.experimental.UtilityClass;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingOutDto;
+import ru.practicum.shareit.booking.dto.BookingShortDto;
+import ru.practicum.shareit.item.ItemMapper;
+import ru.practicum.shareit.user.UserMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@UtilityClass
+public class BookingMapper {
+
+    public static BookingOutDto toBookingDto(Booking booking) {
+        BookingOutDto bookingOutDto = BookingOutDto.builder()
+                .id(booking.getId())
+                .start(booking.getStart())
+                .end(booking.getEnd())
+                .status(booking.getStatus())
+                .item(ItemMapper.toItemDto(booking.getItem()))
+                .booker(UserMapper.toUserDto(booking.getBooker()))
+                .build();
+        return bookingOutDto;
+    }
+
+    public static BookingShortDto toBookingShortDto(Booking booking) {
+        BookingShortDto bookingShortDto = BookingShortDto.builder()
+                .id(booking.getId())
+                .start(booking.getStart())
+                .end(booking.getEnd())
+                .bookerId(booking.getBooker().getId())
+                .build();
+        return bookingShortDto;
+    }
+
+    public static Booking toBooking(BookingDto bookingDto) {
+
+        Booking booking = Booking.builder()
+                .start(bookingDto.getStart())
+                .end(bookingDto.getEnd())
+                .build();
+
+        if (bookingDto.getStatus() == null) {
+            booking.setStatus(Status.WAITING);
+        } else {
+            booking.setStatus(bookingDto.getStatus());
+        }
+        return booking;
+    }
+
+    public static List<BookingOutDto> toBookingDtoList(Iterable<Booking> bookings) {
+        List<BookingOutDto> result = new ArrayList<>();
+
+        for (Booking booking : bookings) {
+            result.add(toBookingDto(booking));
+        }
+        return result;
+    }
+}
---
+++ server/src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -0,0 +1,39 @@
+package ru.practicum.shareit.booking;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    Page<Booking> getAllByBookerIdOrderByStartDesc(long bookerId, Pageable pageable);
+
+    Page<Booking> getAllByBookerIdAndStartBeforeAndEndAfterOrderByStartAsc(long bookerId, LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    Page<Booking> getAllByBookerIdAndEndBeforeOrderByStartDesc(long bookerId, LocalDateTime dateTime, Pageable pageable);
+
+    Page<Booking> getAllByBookerIdAndStartAfterOrderByStartDesc(long bookerId, LocalDateTime dateTime, Pageable pageable);
+
+    Page<Booking> getAllByBookerIdAndStatusOrderByStartDesc(long bookerId, Status status, Pageable pageable);
+
+    Page<Booking> getAllByItemOwnerIdOrderByStartDesc(long ownerId, Pageable pageable);
+
+    Page<Booking> getAllByItemOwnerIdAndStartBeforeAndEndAfterOrderByStartAsc(long ownerId, LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    Page<Booking> getAllByItemOwnerIdAndEndBeforeOrderByStartDesc(long ownerId, LocalDateTime dateTime, Pageable pageable);
+
+    Page<Booking> getAllByItemOwnerIdAndStartAfterOrderByStartDesc(long ownerId, LocalDateTime dateTime, Pageable pageable);
+
+    Page<Booking> getAllByItemOwnerIdAndStatusOrderByStartDesc(long ownerId, Status status, Pageable pageable);
+
+    Optional<Booking> getFirstByItemIdAndStatusAndStartAfterOrderByStartAsc(long itemId, Status status, LocalDateTime dateTime);
+
+    Optional<Booking> getFirstByItemIdAndStatusAndStartBeforeOrderByStartDesc(long itemId, Status status, LocalDateTime dateTime);
+
+    Optional<Booking> getFirstByItemIdAndBookerIdAndStatusAndEndBefore(long itemId, long bookerId, Status status, LocalDateTime dateTime);
+}

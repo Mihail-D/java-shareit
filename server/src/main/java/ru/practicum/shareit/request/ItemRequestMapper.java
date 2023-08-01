2ce8b5bf6830c7d844bb3ba34ff250365680501b
Nuby <nuby.4register@gmail.com>
2023-08-01 13:11:15 +0300

    server-request

---
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
@@ -0,0 +1,42 @@
+package ru.practicum.shareit.request;
+
+import lombok.experimental.UtilityClass;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@UtilityClass
+public class ItemRequestMapper {
+
+    public static ItemRequestDto toItemRequestDto(ItemRequest itemRequest) {
+        ItemRequestDto itemRequestDto = ItemRequestDto.builder()
+                .id(itemRequest.getId())
+                .description(itemRequest.getDescription())
+                .created(itemRequest.getCreated())
+                .items(Collections.emptyList())
+                .build();
+        return itemRequestDto;
+    }
+
+    public static ItemRequest toItemRequest(ItemRequestDto itemRequestDto, User user) {
+        ItemRequest itemRequest = ItemRequest.builder()
+                .description(itemRequestDto.getDescription())
+                .created(LocalDateTime.now())
+                .requester(user)
+                .build();
+        return itemRequest;
+    }
+
+    public static List<ItemRequestDto> toItemRequestDtoList(Iterable<ItemRequest> requests) {
+        List<ItemRequestDto> result = new ArrayList<>();
+
+        for (ItemRequest itemRequest : requests) {
+            result.add(toItemRequestDto(itemRequest));
+        }
+        return result;
+    }
+}
